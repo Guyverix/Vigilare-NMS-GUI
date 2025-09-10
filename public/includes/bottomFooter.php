0e74780a8c8ef81b7d39b971918f731e7c63088c
Christopher Hubbard <chubbard@iwillfearnoevil.com>
2025-09-10 02:24:04 +0000

    Save changes for includes on main page

---
+++ public/includes/bottomFooter.php
@@ -0,0 +1,22 @@
+  <!-- includes/bottomFooter.php -->
+  <footer class="py-4 mt-auto">
+    <div class="container-fluid">
+      <div class="d-flex align-items-center justify-content-between small">
+        <?php echo '<div>&copy; Vigilare ' . date("Y") . '&nbsp </div>'; ?>
+        <div>
+          <a href="/support/privacyPolicy.html">Privacy Policy</a>
+          &nbsp&middot&nbsp;
+          <a href="/support/termsAndConditions.html">Terms &amp; Conditions</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- All JS defined here when possible -->
+  <!-- Remember to be anti-social when possible.  Dont pull from extnernal sources.  An NMS needs to be as stand-alone as possible -->
+  <script src="/js/bootstrapVertical/accordion.js"></script>
+  <!-- <script src="/js/clean-scripts.js"></script>                          -->
+  <script src="/js/cookie/checkCookie.js"></script>
+  <script src="/js/font-awesome/all.min.js" crossorigin="anonymous"></script>
+  <!--  <script src="/js/light-switch-bootstrap-main/switch.js"></script>  -->
+</body>
+</html>
